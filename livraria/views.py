828d55de64233829fe6b401130a585c352bac060
LuizBlume <luiz.fernando.251107@gmail.com>
2025-04-18 21:03:32 -0300

    FEAT: Criação da API REST para a model Autor.

--- livraria/views.py
+++ livraria/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria, Editora
-from livraria.serializers import CategoriaSerializer, EditoraSerializer
+from livraria.models import Categoria, Editora, Autor
+from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
@@ -11,3 +11,7 @@ class CategoriaViewSet(ModelViewSet):
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
+    
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
