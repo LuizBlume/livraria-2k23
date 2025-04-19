d0dddd8611466e38584c42d056b9a7cbbf77a04e
LuizBlume <luiz.fernando.251107@gmail.com>
2025-04-18 22:42:08 -0300

    FEAT: Criação da API de livro e ajustes nas views e serializers.

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from livraria.views import CategoriaViewSet, EditoraViewSet
+from livraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
 
 router = DefaultRouter()
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
+router.register(r"autores", AutorViewSet)
+router.register(r"livros", LivroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- livraria/serializers.py
+++ livraria/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from livraria.models import Categoria, Editora, Autor
+from livraria.models import Categoria, Editora, Autor, Livro
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
@@ -17,3 +17,18 @@ class AutorSerializer(ModelSerializer):
         model = Autor
         fields = "__all__"
         
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
+        
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ["id", "titulo", "preco"]
